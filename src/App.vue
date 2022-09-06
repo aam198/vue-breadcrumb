153271df6e8e0b850472c5b77b25c6f531f5837e
aam198 <aam198@psu.edu>
2022-09-06 11:36:37 -0400

    Vue-breadcrumb inital build

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
+  <MainNav />
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
 </template>
+
+<script>
+import MainNav from '@/components/MainNav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainNav,
+  }
+}
+</script>
+
 
 <style>
 #app {
